99f67470eb3a40aa3508aa1e43edd510153d11a2
Dheeraj yadav <patienceyadav@gmail.com>
2018-12-20 17:51:36 +0530

    manage-school

---
+++ templates/manage-school.php
@@ -0,0 +1,126 @@
+<html>
+<head>
+<title> Manage schools </title>
+
+<style>
+
+</style>
+
+</head>
+
+<body>
+<h3 class="ui header" style="text-align: left;">Manage School</h3>
+
+<br />
+<table class="ui compact celled definition table">
+  <thead>
+    <tr>
+      <th></th>
+      <th>Sr N.</th>
+      <th>School Name</th>
+      <th>Contact Person</th>
+      <th>Contact No</th>
+      <th>Mail Id</th>
+      <th>Address</th>
+      <th>Edit Details</th>
+	  <th>Remark</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td class="collapsing">
+        <div class="ui fitted slider checkbox">
+          <input type="checkbox"> <label></label>
+        </div>
+      </td>
+      <td>1</td>
+      <td>VIT Chennai</td>
+      <td>Asha</td>
+      <td>1234567890</td>
+      <td>vitchennai@vit.ac.in</td>
+      <td>Chennai, India</td>
+      <td>
+	  <button class="ui primary basic button">Edits</button>
+	  </td>
+	  <td>
+		<div class="ui form">
+			<div class="field">
+			<textarea rows="1"></textarea>
+		</div>
+	  </td>
+    </tr>
+    <tr>
+      <td class="collapsing">
+        <div class="ui fitted slider checkbox">
+          <input type="checkbox"> <label></label>
+        </div>
+      </td>
+      <td>2</td>
+      <td>VIT Vellore</td>
+      <td>Jamie Harington</td>
+      <td>8965782345</td>
+      <td>jamieharingonton@yahoo.com</td>
+      <td>Vellore India</td>
+      <td>
+	  <button class="ui primary basic button">Edits</button>
+	  </td>
+	  <td>
+		<div class="ui form">
+			<div class="field">
+			<textarea rows="1"></textarea>
+		</div>
+	  </td>
+    </tr>
+    <tr>
+      <td class="collapsing">
+        <div class="ui fitted slider checkbox">
+          <input type="checkbox"> <label></label>
+        </div>
+      </td>
+      <td>3</td>
+      <td>GLA University</td>
+      <td>Jill Lewis</td>
+      <td>3456112014</td>
+      <td>jilsewris22@yahoo.com</td>
+      <td>Mathura, UP</td>
+      <td>
+	  <button class="ui primary basic button">Edits</button>
+	  </td>
+	  <td>
+		<div class="ui form">
+			<div class="field">
+			<textarea rows="1"></textarea>
+		</div>
+	  </td>
+    </tr>
+  </tbody>
+  <tfoot class="full-width">
+    <tr>
+      <th></th>
+      <th colspan="8">
+        <div class="ui right floated pagination menu">
+        <a class="icon item">
+          <i class="left chevron icon"></i>
+        </a>
+        <a class="item">1</a>
+        <a class="item">2</a>
+        <a class="item">3</a>
+        <a class="item">4</a>
+        <a class="icon item">
+          <i class="right chevron icon"></i>
+        </a>
+      </div>
+        <div class="ui small button">
+          Approve
+        </div>
+        <div class="ui small  disabled button">
+          Approve All
+        </div>
+      </th>
+    </tr>
+  </tfoot>
+  
+  
+</table>
+</body>
+</html>

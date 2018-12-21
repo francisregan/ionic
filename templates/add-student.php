d7819e6e4885a1d6ca08fdc6dd1fbee11eb7d4b7
Dheeraj yadav <patienceyadav@gmail.com>
2018-12-21 18:33:36 +0530

    friday work

---
+++ templates/add-student.php
@@ -0,0 +1,76 @@
+<html>
+<head>
+<title> Add Student </title>
+</head>
+
+<body>
+
+<form class="ui form">
+<br />
+<h3 class="ui dividing header" style="text-align: left;">Add Student</h3>
+<br />
+<div style="align:center">
+<div>
+  <div class="field">
+    
+    <div class="two fields">
+      <div class="eight wide field">
+      <label>Student Name</label>
+      </div>
+      <div class="eight wide field">
+        <input type="text" name="shipping[name]" placeholder="Name of Student">
+      </div>
+    </div>
+  </div>
+  <div class="field">
+    <div class="two fields">
+      <div class="eight wide field">
+      <label>Contact No</label>
+      </div>
+      <div class="eight wide field">
+        <input type="text" name="shipping[Contact Number]" placeholder="Contact Number">
+      </div>
+    </div>
+  </div>
+  <div class="field">
+    <div class="two fields">
+      <div class="eight wide field">
+      <label>Mail Id</label>
+      </div>
+      <div class="eight wide field">
+        <input type="text" name="shipping[Mail Id]" placeholder="abc@gmail.com">
+      </div>
+    </div>
+  </div>
+  <div class="field">
+    <div class="two fields">
+      <div class="eight wide field">
+      <label>School</label>
+      </div>
+      <div class="eight wide field">
+        <input type="text" name="shipping[School]" placeholder="Name of School">
+      </div>
+    </div>
+  </div>
+  <div class="field">
+    <div class="two fields">
+      <div class="eight wide field">
+      <label>Batch</label>
+      </div>
+      <div class="eight wide field">
+        <input type="text" name="shipping[Batch]" placeholder="Batch">
+      </div>
+    </div>
+  </div>
+  </div>
+  <h4 class="ui dividing header"></h4>
+
+  <div class="ui button" tabindex="0">Add Student</div>
+  </div>
+</div>
+
+
+
+</form>
+</body>
+</html>
---
+++ templates/manage-teacher.php
@@ -0,0 +1,126 @@
+<html>
+<head>
+<title> Manage Trainer </title>
+
+<style>
+
+</style>
+
+</head>
+
+<body>
+<h3 class="ui header" style="text-align: left;">Manage Trainer</h3>
+
+<br />
+<table class="ui compact celled definition table">
+  <thead>
+    <tr>
+      <th></th>
+      <th>T. Id</th>
+      <th>Trainer Name</th>
+      <th>Contact No</th>
+      <th>Mail Id</th>
+      <th>Specialization</th>
+      <th>School</th>
+      <th>Edit Details</th>
+	  <th>Remark</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td class="collapsing">
+        <div class="ui fitted slider checkbox">
+          <input type="checkbox"> <label></label>
+        </div>
+      </td>
+      <td>1</td>
+      <td>Asha</td>
+      <td>1234567890</td>
+      <td>asha@gmail.com</td>
+      <td>Network Security</td>
+      <td>Vit Chennai</td>
+      <td>
+	  <button class="ui primary basic button">Edits</button>
+	  </td>
+	  <td>
+		<div class="ui form">
+			<div class="field">
+			<textarea rows="1"></textarea>
+		</div>
+	  </td>
+    </tr>
+    <tr>
+      <td class="collapsing">
+        <div class="ui fitted slider checkbox">
+          <input type="checkbox"> <label></label>
+        </div>
+      </td>
+      <td>2</td>
+      <td>Premalatha</td>
+      <td>6789012345</td>
+      <td>prema@gmail.com</td>
+      <td>Data Mining</td>
+      <td>Bharath University</td>
+      <td>
+	  <button class="ui primary basic button">Edits</button>
+	  </td>
+	  <td>
+		<div class="ui form">
+			<div class="field">
+			<textarea rows="1"></textarea>
+		</div>
+	  </td>
+    </tr>
+    <tr>
+      <td class="collapsing">
+        <div class="ui fitted slider checkbox">
+          <input type="checkbox"> <label></label>
+        </div>
+      </td>
+      <td>3</td>
+      <td>Roshan</td>
+      <td>9854678943</td>
+      <td>roshan@gmail.com</td>
+      <td>Operating System</td>
+      <td>St. Joseph</td>
+      <td>
+	  <button class="ui primary basic button">Edits</button>
+	  </td>
+	  <td>
+		<div class="ui form">
+			<div class="field">
+			<textarea rows="1"></textarea>
+		</div>
+	  </td>
+    </tr>
+  </tbody>
+  <tfoot class="full-width">
+    <tr>
+      <th></th>
+      <th colspan="8">
+        <div class="ui right floated pagination menu">
+        <a class="icon item">
+          <i class="left chevron icon"></i>
+        </a>
+        <a class="item">1</a>
+        <a class="item">2</a>
+        <a class="item">3</a>
+        <a class="item">4</a>
+        <a class="icon item">
+          <i class="right chevron icon"></i>
+        </a>
+      </div>
+        <div class="ui small button">
+          Approve
+        </div>
+        <div class="ui small  disabled button">
+          Approve All
+        </div>
+      </th>
+    </tr>
+  </tfoot>
+  
+  
+</table>
+</body>
+</html>

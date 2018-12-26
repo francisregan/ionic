c8e1224fc8bc822996519bf5ac827a6e2db42e24
DIKSHA SHARMA <sharma157diksha@gmail.com>
2018-12-26 18:41:22 +0530

    Add School By Leena

--- templates/add-school.php
+++ templates/add-school.php
@@ -5,120 +5,71 @@
 
 <body>
 
-<form class="ui form">
-<h3 class="ui header" style="text-align: left;">Add student</h3>
-
-<br /><br />
+<form class="ui form" action="school" method="post" >
+<br />
+<h3 class="ui dividing header" style="text-align: left;">Add School</h3>
+<br />
 <div style="align:center">
+<div>
   <div class="field">
-    <label>Name</label>
+     <div class="two fields">
+      <div class="eight wide field">
+      <label>School Name</label>
+      </div>
+      <div class="eight wide field">
+        <input type="text" name="sname" placeholder="Name of School">
+      </div>
+    </div>
+  </div>
+  <div class="field">
     <div class="two fields">
-      <div class="field">
-        <input type="text" name="shipping[first-name]" placeholder="First Name">
+      <div class="eight wide field">
+      <label>Contact No</label>
       </div>
-      <div class="field">
-        <input type="text" name="shipping[last-name]" placeholder="Last Name">
+      <div class="eight wide field">
+        <input type="text" name="sphoneno" placeholder="Contact Number">
       </div>
     </div>
   </div>
   <div class="field">
-    <label>Billing Address</label>
-    <div class="fields">
-      <div class="twelve wide field">
-        <input type="text" name="shipping[address]" placeholder="Street Address">
+    <div class="two fields">
+      <div class="eight wide field">
+      <label>Contact Person</label>
       </div>
-      <div class="four wide field">
-        <input type="text" name="shipping[address-2]" placeholder="Apt #">
+      <div class="eight wide field">
+        <input type="text" name="scontactperson" placeholder="Name of person">
       </div>
     </div>
   </div>
-  
-    <div class="field">
-      <label>Country</label>
-      <div class="ui fluid search selection dropdown">
-        <input type="hidden" name="country">
-        <i class="dropdown icon"></i>
-        <div class="default text">Select Country</div>
-        <div class="menu">
-            <div class="item" data-value="af"><i class="af flag"></i>Afghanistan</div>
-            <div class="item" data-value="ax"><i class="ax flag"></i>Aland Islands</div>
-            <div class="item" data-value="al"><i class="al flag"></i>Albania</div>
-            <div class="item" data-value="dz"><i class="dz flag"></i>Algeria</div>
-            <div class="item" data-value="as"><i class="as flag"></i>American Samoa</div>
-            <div class="item" data-value="ad"><i class="ad flag"></i>Andorra</div>
-       </div>
-    </div>
-  </div>
-  <h4 class="ui dividing header">Billing Information</h4>
   <div class="field">
-    <label>Card Type</label>
-    <div class="ui selection dropdown">
-      <input type="hidden" name="card[type]">
-      <div class="default text">Type</div>
-      <i class="dropdown icon"></i>
-      <div class="menu">
-        <div class="item" data-value="visa">
-          <i class="visa icon"></i>
-          Visa
-        </div>
-        <div class="item" data-value="amex">
-          <i class="amex icon"></i>
-          American Express
-        </div>
-        <div class="item" data-value="discover">
-          <i class="discover icon"></i>
-          Discover
-        </div>
+    <div class="two fields">
+      <div class="eight wide field">
+      <label>Mail id</label>
+      </div>
+      <div class="eight wide field">
+        <input type="text" name="smailid" placeholder="abc@gmail.com">
       </div>
     </div>
   </div>
-  <div class="fields">
-    <div class="seven wide field">
-      <label>Card Number</label>
-      <input type="text" name="card[number]" maxlength="16" placeholder="Card #">
-    </div>
-    <div class="three wide field">
-      <label>CVC</label>
-      <input type="text" name="card[cvc]" maxlength="3" placeholder="CVC">
-    </div>
-    <div class="six wide field">
-      <label>Expiration</label>
-      <div class="two fields">
-        <div class="field">
-          <select class="ui fluid search dropdown" name="card[expire-month]">
-            <option value="">Month</option>
-            <option value="1">January</option>
-            <option value="2">February</option>
-            <option value="3">March</option>
-            <option value="4">April</option>
-            <option value="5">May</option>
-            <option value="6">June</option>
-            <option value="7">July</option>
-            <option value="8">August</option>
-            <option value="9">September</option>
-            <option value="10">October</option>
-            <option value="11">November</option>
-            <option value="12">December</option>
-          </select>
-        </div>
-        <div class="field">
-          <input type="text" name="card[expire-year]" maxlength="4" placeholder="Year">
-        </div>
+ 
+  <div class="field">
+    <div class="two fields">
+      <div class="eight wide field">
+      <label>School Address</label>
+      </div>
+      <div class="eight wide field">
+        <input type="text" name="saddress" placeholder="Enter the address">
       </div>
     </div>
   </div>
-   <h4 class="ui dividing header">Receipt</h4>
-   
-   <div class="ui segment">
-    <div class="field">
-      <div class="ui toggle checkbox">
-        <input type="checkbox" name="gift" tabindex="0" class="hidden">
-        <label>Do not include a receipt in the package</label>
-      </div>
-    </div>
+
   </div>
-  <div class="ui button" tabindex="0">Submit Order</div>
-  </div>
+  <h4 class="ui dividing header"></h4>
+ 
+<form class="ui form" action="school" method="post" >
+<input id="submitBtn" type="submit" class="ui button" style="background-color:#2185d0" name="school" value="Add Record" ></input>
+</div>
+</div>
 </form>
 </body>
 </html>

9a76f566bdf87fdaaffe1c59b75db42709d00b64
Dheeraj yadav <patienceyadav@gmail.com>
2019-02-28 16:06:46 +0530

    student login

--- src/controllers/LoginController.php
+++ src/controllers/LoginController.php
@@ -19,21 +19,27 @@ class LoginController
   public function login($request, $response, $args) {
     //echo "in here";
     $data = $request->getParsedBody();
-    $email = filter_var($data['login'], FILTER_SANITIZE_STRING);
+    $userid = filter_var($data['login'], FILTER_SANITIZE_STRING);
     $password = filter_var($data['password'], FILTER_SANITIZE_STRING);
     $type = filter_var($data['charactertype'], FILTER_SANITIZE_STRING);
-    $charactertype = filter_var($data['charactertype'], FILTER_SANITIZE_STRING);
-    $querystr = "SELECT * FROM ioniccloud.login where email='$email' AND password='$password' AND type='$type'";
-    $result = $this->container->db->query($querystr);
-    
-    if ($result->num_rows > 0) {
-      while($row = mysqli_fetch_array($result)) {
-        $_SESSION['user'] = $row['name'];
-        $_SESSION['email'] = $row['email'];
-        $_SESSION['type']=$row['type'];
-       
+    if($type == 1){
+      $studentid = ltrim(substr($userid,7,4),'\0');
+      $schoolid = ltrim(substr($userid,3,4),'\0');
+      $result = $this->container->db->query("SELECT * FROM ioniccloud.student where student_id='$studentid' AND school='$schoolid';");
+      if ($result->num_rows > 0) {
+        while($row = mysqli_fetch_array($result)) {
+          $_SESSION['user'] = $row['student_name'];
+          $_SESSION['student_id'] = $row['student_id'];
+          $_SESSION['school_id'] = $row['school'];
+          $_SESSION['type']= $type;
+          $lastword = substr(preg_replace('/\s+/', '', $row['student_name']),-3);
+          $generatedPassword = $lastword."@ionic";
+        }
+        $passwordresult = strcmp($password,$generatedPassword);
+        if($passwordresult == 0){
+          return $this->container->renderer->render($response, 'index.php', $args);
+        }
       }
-      return $this->container->renderer->render($response, 'index.php', $args);
     }
     echo("<script>window.alert('USERNAME OR PASSWORD IS INCORRECT');</script>");
     return $this->container->renderer->render($response, 'login.php', $args);
